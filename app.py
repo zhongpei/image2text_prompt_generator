c9d71abb3e8db12dd297d34541099b846bb86912
fofo <zhongpei@vip.qq.com>
2023-04-16 20:40:52 +0800

    fix

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import gradio as gr
 import torch
-import mdtex2html
+
+from utils.chatglm import models as chatglm_models
 from utils.exif import get_image_info
 from utils.generator import generate_prompt
 from utils.image2text import git_image2text, w14_image2text, clip_image2text
 from utils.translate import en2zh as translate_en2zh
 from utils.translate import zh2en as translate_zh2en
-from utils.chatglm import chat2text
-from utils.chatglm import models as chatglm_models
-
+from ui.chat import chatglm_ui
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
@@ -61,65 +60,7 @@ def translate_input(text: str, chatglm_text: str) -> str:
 
 with gr.Blocks(title="Prompt生成器") as block:
     with gr.Column():
-        with gr.Tab('Chat'):
-            def revise(history, latest_message):
-                history[-1] = (history[-1][0], latest_message)
-                return history, ''
 
-
-            def revoke(history):
-                if len(history) >= 1:
-                    history.pop()
-                return history
-
-
-            def interrupt(allow_generate):
-                allow_generate[0] = False
-
-
-            def reset_state():
-                return [], []
-
-
-            with gr.Row():
-                with gr.Column(scale=4):
-                    chatbot = gr.Chatbot(elem_id="chat-box", show_label=False).style(height=800)
-                with gr.Column(scale=1):
-                    with gr.Row():
-                        max_length = gr.Slider(32, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
-                        top_p = gr.Slider(0.01, 1, value=0.7, step=0.01, label="Top P", interactive=True)
-                        temperature = gr.Slider(0.01, 5, value=0.95, step=0.01, label="Temperature", interactive=True)
-                    with gr.Row():
-                        query = gr.Textbox(show_label=False, placeholder="Prompts", lines=4).style(container=False)
-                        generate_button = gr.Button("生成")
-                    with gr.Row():
-                        continue_message = gr.Textbox(
-                            show_label=False, placeholder="Continue message", lines=2).style(container=False)
-                        continue_btn = gr.Button("续写")
-                        revise_message = gr.Textbox(
-                            show_label=False, placeholder="Revise message", lines=2).style(container=False)
-                        revise_btn = gr.Button("修订")
-                        revoke_btn = gr.Button("撤回")
-                        interrupt_btn = gr.Button("终止生成")
-                        reset_btn = gr.Button("清空")
-
-            history = gr.State([])
-            allow_generate = gr.State([True])
-            blank_input = gr.State("")
-            reset_btn.click(reset_state, outputs=[chatbot, history], show_progress=True)
-            generate_button.click(
-                chatglm_models.chatglm.predict_continue,
-                inputs=[query, blank_input, max_length, top_p, temperature, allow_generate, history],
-                outputs=[chatbot, query]
-            )
-            revise_btn.click(revise, inputs=[history, revise_message], outputs=[chatbot, revise_message])
-            revoke_btn.click(revoke, inputs=[history], outputs=[chatbot])
-            continue_btn.click(
-                chatglm_models.chatglm.predict_continue,
-                inputs=[query, continue_message, max_length, top_p, temperature, allow_generate, history],
-                outputs=[chatbot, query, continue_message]
-            )
-            interrupt_btn.click(interrupt, inputs=[allow_generate])
         with gr.Tab('文本生成'):
             with gr.Row():
                 input_text = gr.Textbox(lines=6, label='你的想法', placeholder='在此输入内容...')
@@ -202,6 +143,9 @@ with gr.Blocks(title="Prompt生成器") as block:
                     value=0.85,
                     label="Character Tags Threshold",
                 )
+
+        chatglm_ui()
+
     img_prompter_btn.click(
         fn=image_generate_prompter,
         inputs=[
---
+++ ui/chat.py
@@ -0,0 +1,64 @@
+import gradio as gr
+import torch
+
+from utils.chatglm import models as chatglm_models
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def chatglm_ui():
+    with gr.Tab('Chat'):
+        def revise(history, latest_message):
+            history[-1] = (history[-1][0], latest_message)
+            return history, ''
+
+        def revoke(history):
+            if len(history) >= 1:
+                history.pop()
+            return history
+
+        def interrupt(allow_generate):
+            allow_generate[0] = False
+
+        def reset_state():
+            return [], []
+
+        with gr.Row():
+            with gr.Column(scale=4):
+                chatbot = gr.Chatbot(elem_id="chat-box", show_label=False).style(height=800)
+            with gr.Column(scale=1):
+                with gr.Row():
+                    max_length = gr.Slider(32, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
+                    top_p = gr.Slider(0.01, 1, value=0.7, step=0.01, label="Top P", interactive=True)
+                    temperature = gr.Slider(0.01, 5, value=0.95, step=0.01, label="Temperature", interactive=True)
+                with gr.Row():
+                    query = gr.Textbox(show_label=False, placeholder="Prompts", lines=4).style(container=False)
+                    generate_button = gr.Button("生成")
+                with gr.Row():
+                    continue_message = gr.Textbox(
+                        show_label=False, placeholder="Continue message", lines=2).style(container=False)
+                    continue_btn = gr.Button("续写")
+                    revise_message = gr.Textbox(
+                        show_label=False, placeholder="Revise message", lines=2).style(container=False)
+                    revise_btn = gr.Button("修订")
+                    revoke_btn = gr.Button("撤回")
+                    interrupt_btn = gr.Button("终止生成")
+                    reset_btn = gr.Button("清空")
+
+        history = gr.State([])
+        allow_generate = gr.State([True])
+        blank_input = gr.State("")
+        reset_btn.click(reset_state, outputs=[chatbot, history], show_progress=True)
+        generate_button.click(
+            chatglm_models.chatglm.predict_continue,
+            inputs=[query, blank_input, max_length, top_p, temperature, allow_generate, history],
+            outputs=[chatbot, query]
+        )
+        revise_btn.click(revise, inputs=[history, revise_message], outputs=[chatbot, revise_message])
+        revoke_btn.click(revoke, inputs=[history], outputs=[chatbot])
+        continue_btn.click(
+            chatglm_models.chatglm.predict_continue,
+            inputs=[query, continue_message, max_length, top_p, temperature, allow_generate, history],
+            outputs=[chatbot, query, continue_message]
+        )
+        interrupt_btn.click(interrupt, inputs=[allow_generate])
