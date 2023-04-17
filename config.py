50067ee142e96b9d362b08a215c55827b42ab0e9
fofo <zhongpei@vip.qq.com>
2023-04-17 09:10:23 +0800

    fix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="IPG_CONF",
-    settings_files=['settings.toml', '.secrets.toml'],
+    settings_files=['settings.toml',  '.secrets.toml'],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
