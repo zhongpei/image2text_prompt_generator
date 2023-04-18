9f437571c00840ba983fb8fbefbe17b1451de33c
fofo <zhongpei@vip.qq.com>
2023-04-18 21:39:18 +0800

    fix

--- imagetools/tags.py
+++ imagetools/tags.py
@@ -4,13 +4,14 @@ from utils.translate import en2zh as translate_en2zh
 import os
 
 
-def get_tag_files(input_dir):
-    def get_tag_from_file(fn):
-        with open(fn, "r") as f:
-            data = f.read().split(",")
-            tags = [d for d in data if d.strip() if len(d.strip()) > 0]
-        return tags
+def get_tag_from_file(fn: str) -> List[str]:
+    with open(fn, "r") as f:
+        data = f.read().split(",")
+        tags = [d.strip() for d in data if len(d.strip()) > 0]
+    return tags
 
+
+def get_tag_files(input_dir: str) -> Dict[str, List[str]]:
     tag_files = {}
     for root, dirs, files in os.walk(input_dir, topdown=False):
         for name in files:
@@ -22,17 +23,19 @@ def get_tag_files(input_dir):
     return tag_files
 
 
-def load_translated_tags(input_dir):
+def load_translated_tags(input_dir: str):
     if not os.path.exists(input_dir) or not os.path.isdir(input_dir):
         return {}
     tag_files = get_tag_files(input_dir)
-    tags = {}
+    tags = []
     for _, v in tag_files.items():
-        tags.update(translate_tags(v))
-    return tags
+        tags += v
+    tags = list(set(tags))
+    return translate_tags(tags)
 
 
 def translate_tags(tags: List[str]) -> Dict[str, str]:
     """Translate a tag to a human readable string."""
     tags_zh = [translate_en2zh(tag) for tag in tags]
     return dict(zip(tags, tags_zh))
+
