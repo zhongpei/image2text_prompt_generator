f6089da48179c2dcc2bc5bfcb23e07496e1ba98a
fofo <qqqeee@gmail.com>
2023-04-18 15:14:30 +0800

    fix chatglm on cpu

--- api_offline.bat
+++ api_offline.bat
@@ -1,21 +1,17 @@
-
-
 @echo off
 
-
-
+set SETTINGS_FILE_FOR_DYNACONF="settings-offline.toml"
 
 set PYTHON=".\system\python\Scripts\python.exe"
 set GIT=
 set VENV_DIR=.\system\python
 
 :activate_venv
-set SETTINGS_FILE_FOR_DYNACONF="settings-offline.toml"
 set PYTHON="%VENV_DIR%\Python.exe"
 echo venv %PYTHON%
 
 
 :launch
-%PYTHON% app.py %*
+%PYTHON% app_api.py %*
 pause
 exit /b
