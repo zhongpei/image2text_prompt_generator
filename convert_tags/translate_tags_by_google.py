e3f7b09b3eda863b18e9243d6d46733d17c05792
fofo <zhongpei@vip.qq.com>
2023-04-19 11:29:22 +0800

    fix

--- convert_tags/translate_tags_by_google.py
+++ convert_tags/translate_tags_by_google.py
@@ -7,11 +7,14 @@ translator = Translator()
 result = translator.translate('hello world','zh-cn')
 print(result.text)
 lines = []
+
 with open("all.txt","r") as f:
     lines = f.readlines()
-with open("all_zh.text","a+") as f:
+
+with open("all_zh.text","w+") as f:
     for l in lines:
         l = l.replace("_"," ").strip()
         result = translator.translate(l,'zh-cn')
         print("{}={}".format(l,result.text))
-        f.write("{}={}".format(l,result))
+        f.write("{}={}\n".format(l,result.text))
+        f.flush()
