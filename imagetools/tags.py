56d82f469450ac9e636b226c3790f56b4c08629c
fofo <zhongpei@vip.qq.com>
2023-04-20 19:01:38 +0800

    fix

--- imagetools/tags.py
+++ imagetools/tags.py
@@ -90,7 +90,8 @@ def gen_wd14_tags_files(
                 general_threshold=general_threshold,
                 character_threshold=character_threshold
             )
-            tags = ",".join(tags.strip(",")[:w14_tags_max_count])
+
+            tags = ",".join(tags.split(",")[:w14_tags_max_count])
             print(f'{image_fn} -> {tags}')
             insert_tag2file(new_tags=tags, tags_pos=tags_pos, fn=tags_fn)
             output.append(tags_fn)
