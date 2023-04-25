f6dbeeb0acc66aabd6456865af640dd069a03368
fofo <zhongpei@vip.qq.com>
2023-04-25 13:33:14 +0800

    fix

--- ui/chain.py
+++ ui/chain.py
@@ -34,6 +34,10 @@ def get_vs_list(vs_path):
 
 
 def get_answer(query, history):
+    if not local_doc_qa.is_initialized() or not local_doc_qa.is_active():
+        history[-1][-1] += ["""知识库未成功加载，请到页面左上角"知识库"选项卡中重新选择后加载"""]
+        return history
+    
     resp, history = local_doc_qa.get_knowledge_based_answer(
         query=query,
         chat_history=history
